5cd203256f927e8e70c4fb6f251902db71b23929
gram <git@orsinium.dev>
2022-09-11 09:42:31 +0200

    render html

--- sdk/cli.py
+++ sdk/cli.py
@@ -5,6 +5,7 @@ from pathlib import Path
 import sys
 from typing import Callable, Mapping, NoReturn, TextIO
 from .post import Post
+from jinja2 import Environment, FileSystemLoader
 
 try:
     import ipdb as pdb
@@ -13,6 +14,15 @@ except ImportError:
 
 Command = Callable[[], int]
 ROOT = Path(__file__).parent.parent
+TEMPLATES_PATH = ROOT / 'templates'
+jinja_env = Environment(
+    loader=FileSystemLoader(TEMPLATES_PATH),
+)
+TITLES = {
+    'index.html': 'Python etc.',
+    # 'peps.html': 'Python etc. / PEPs',
+    'peps.html': 'Python etc. / PEPs',
+}
 
 
 def get_posts() -> list[Post]:
@@ -28,14 +38,41 @@ def get_posts() -> list[Post]:
 
 def cmd_table() -> int:
     for post in get_posts():
-        pep = f'PEP {post.pep:<4}' if post.pep else ' ' * 8
+        pep = f'PEP {post.pep[0]:<4}' if post.pep else ' ' * 8
         python = f'{post.python:<4}' if post.python else ' ' * 4
         print(f'{post.published} | {pep} | {python} | {post.title}')
     return 0
 
 
+def cmd_html() -> int:
+    posts = get_posts()
+    (ROOT / 'public' / 'posts').mkdir(exist_ok=True, parents=True)
+    render_html('index')
+    render_html('posts', posts=posts, title='all posts')
+    for post in posts:
+        render_post(post)
+    return 0
+
+
+def render_html(slug: str, **kwargs) -> None:
+    print(slug)
+    template = jinja_env.get_template(f'{slug}.html.j2')
+    content = template.render(**kwargs)
+    html_path = ROOT / 'public' / f'{slug}.html'
+    html_path.write_text(content, encoding='utf8')
+
+
+def render_post(post: Post) -> None:
+    print(f'posts/{post.slug}')
+    template = jinja_env.get_template('post.html.j2')
+    content = template.render(post=post, title=post.title)
+    html_path = ROOT / 'public' / 'posts' / f'{post.slug}.html'
+    html_path.write_text(content, encoding='utf8')
+
+
 COMMANDS: Mapping[str, Command] = {
     'table': cmd_table,
+    'html': cmd_html,
 }
 
 
--- sdk/post.py
+++ sdk/post.py
@@ -1,19 +1,28 @@
 from __future__ import annotations
-from dataclasses import dataclass
 from datetime import date
 from functools import cached_property
 from pathlib import Path
+import attr
 import yaml
+from markdown_it import MarkdownIt
+
+md_parser = MarkdownIt()
 
 
-@dataclass(frozen=True)
+def wrap_list(x: object) -> list:
+    if isinstance(x, list):
+        return x
+    return [x]
+
+
+@attr.s(auto_attribs=True, frozen=True)
 class Post:
     path: Path
     markdown: str
     author: str
-    qname: list[str]
+    qname: list[str] = attr.ib(factory=list, converter=wrap_list)
+    pep: list[int] = attr.ib(factory=list, converter=wrap_list)
     published: date | None = None
-    pep: int | None = None
     python: str | None = None
 
     @classmethod
@@ -29,3 +38,15 @@ class Post:
     def title(self) -> str:
         first_line = self.markdown.lstrip().split('\n', maxsplit=1)[0]
         return first_line.removeprefix('# ')
+
+    @cached_property
+    def md_content(self) -> str:
+        return self.markdown.lstrip().split('\n', maxsplit=1)[-1]
+
+    @cached_property
+    def html_content(self) -> str:
+        return md_parser.render(self.md_content)
+
+    @property
+    def slug(self) -> str:
+        return self.path.stem
