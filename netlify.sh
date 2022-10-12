742719cd21cbdf0a52ad6bbea4261511016ca6ff
gram <git@orsinium.dev>
2022-10-12 13:48:36 +0200

    don't use task on netlify

--- netlify.sh
+++ netlify.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-
 # This script is used by netlify
-
-sh -c "$(curl --location https://taskfile.dev/install.sh)" -- -d
-./bin/task sdk -- html
+python3 -m pip install .
+python3 -m sdk html
