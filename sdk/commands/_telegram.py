38cb35b60126ba3ed5b88fcab20a832e1ca7f1fc
Vadim Pushtaev <pushtaev.vm@gmail.com>
2022-11-13 00:42:01 +0200

    next in sequence warning

--- sdk/commands/_telegram.py
+++ sdk/commands/_telegram.py
@@ -18,5 +18,18 @@ class TelegramCommand(Command):
         parser.add_argument('post', type=Path)
 
     def run(self) -> int:
-        self.print(Post.from_path(self.args.post).telegram_markdown)
+        post = Post.from_path(self.args.post)
+        self.print(post.telegram_markdown)
+
+        if post.sequence:
+            index = post.self_in_sequence().index
+            if len(post.sequence.posts) > index + 1:
+                next_post = post.sequence.posts[index + 1]
+                if not next_post.delay_allowed:
+                    self.warn(
+                        f'Next post must be posted immediately: '
+                        f'python -m sdk telegram posts/{next_post.path.name}'
+                    )
+
+
         return 0
