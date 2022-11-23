5642bf96b31b0de817ef9db144b6a5ab17984957
Vadim Pushtaev <pushtaev.vm@gmail.com>
2022-11-23 19:10:59 +0200

    heapq

--- markdown_from_result_json.py
+++ markdown_from_result_json.py
@@ -1,32 +1,52 @@
 import json
 import sys
 from pathlib import Path
+from textwrap import dedent
 
 
-meta = json.loads(Path('result.json').read_text('utf8'))
-target_id = int(sys.argv[1])
+def main():
+    meta = json.loads(Path('result.json').read_text('utf8'))
+    target_id = int(sys.argv[1])
 
-filtered = [x for x in meta['messages'] if x['id'] == target_id]
-assert len(filtered) == 1, (
-    "Found not exactly one message (but {}) with id {}".format(len(filtered), target_id)
-)
-target = filtered[0]
+    filtered = [x for x in meta['messages'] if x['id'] == target_id]
+    assert len(filtered) == 1, (
+        'Found not exactly one message (but {}) with id {}'.format(len(filtered), target_id)
+    )
+    target = filtered[0]
+    date = target['date'][0:10]
 
-for line in target['text']:
-    if isinstance(line, str):
-        print(line, end='')
-    elif isinstance(line, dict):
-        if line['type'] == 'bold':
-            print("*{}*".format(line['text']), end='')
-        elif line['type'] == 'italic':
-            print("_{}_".format(line['text']), end='')
-        elif line['type'] == 'code':
-            print("`{}`".format(line['text']), end='')
-        elif line['type'] == 'pre':
-            print("```python")
-            print(line['text'])
-            print("```")
-        else:
-            raise ValueError("Unknown line type: {}".format(line['type']))
-    else:
-        raise ValueError("Unknown line type: {}".format(type(line)))
+    with open(f'posts/__{target_id}__.md', 'w', encoding='utf8') as f:
+        f.write(dedent(f'''
+        ---
+        published: {date}
+        id: {target_id}
+        author: pushtaev
+        ---
+        
+        # ...
+        '''))
+
+        for line in target['text']:
+            if isinstance(line, str):
+                f.write(line)
+            elif isinstance(line, dict):
+                if line['type'] == 'bold':
+                    f.write('*{}*'.format(line['text']))
+                elif line['type'] == 'italic':
+                    f.write('_{}_'.format(line['text']))
+                elif line['type'] == 'code':
+                    f.write('`{}`'.format(line['text']))
+                elif line['type'] == 'pre':
+                    f.write('```python\n')
+                    f.write(line['text'] + '\n')
+                    f.write('```\n')
+                elif line['type'] == 'text_link':
+                    f.write('[{}]({})'.format(line['text'], line['href']))
+                else:
+                    raise ValueError('Unknown line type: {}'.format(line['type']))
+            else:
+                raise ValueError('Unknown line type: {}'.format(type(line)))
+
+
+if __name__ == '__main__':
+    main()
